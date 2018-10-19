b27516e87d1a0dcbfb0256fd5f390237d0ec3ed0
Poomon Nateluercha <31557298+poomonnateluercha@users.noreply.github.com>
2018-10-20 01:21:59 +0700

    Update reference

--- Wrapper.py
+++ Wrapper.py
@@ -8,6 +8,8 @@ space = ' '
 #response = google_images_download.googleimagesdownload()
 #absolute_image_paths = response.download({'keywords':'Nicolas Cage', 'limit':5, 'format':'jpg'})
 
+#Credit to phihag for answer on how to change folder name with python 
+#(https://stackoverflow.com/questions/8735312/how-to-change-folder-names-in-python)
 for folder in os.listdir(basedir):
     if folder.count(space) != 0:
         print(folder)
